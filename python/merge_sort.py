45de73df05088a26ad0456142a4d6d747bb9f3ad
yannliao <trembleliao@gmail.com>
2016-03-22 16:16:53 +0800

    add docstring

--- python/merge_sort.py
+++ python/merge_sort.py
@@ -1,4 +1,15 @@
+"""
+  Merge Sort
+  CLRS Chapter 2.3
+"""
+
 def merge(L, R):
+    """
+    Merge two sorted subsequences into one sorted tree
+    Param L: the first sorted list
+    Param R: the second sorted list
+    Retrun result: the merged list of L and R which is sorted
+    """
     i = j = 0
     k = p
     result = []
@@ -15,6 +26,11 @@ def merge(L, R):
 
 
 def merge_sort(array):
+    """
+    Sort the array in ascending order, then a sorted list is returned.
+    Param array: sequences will be sorted.
+    Return result: the sorted list.
+    """
     if len(array) <= 1:
         return array
     mid = int(len(array)/2)
