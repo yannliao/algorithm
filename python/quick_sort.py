e4bf7f5fb46d9be8eeb2ce9d447df03067ce6b10
yannliao <trembleliao@gmail.com>
2016-05-10 10:15:07 +0800

    randomized edition of quicksort

--- python/quick_sort.py
+++ python/quick_sort.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+#
+import random
 """
   Quick Sort
   CLRS Chapter 7.1
@@ -18,7 +20,7 @@ def partition(A, p, r):
       i = i + 1
       A[i], A[j] = A[j], A[i]
   A[i + 1], A[r] = A[r], A[i + 1]
-  print('A={0}, x={1}, i+1={2}, p={3}, r={4} '.format(A, x,  i + 1, p, r))
+  # print('A={0}, x={1}, i+1={2}, p={3}, r={4} '.format(A, x,  i + 1, p, r))
   return i + 1
 
 
@@ -31,10 +33,30 @@ def quicksort(A, p, r):
   """
   if p < r:
     q = partition(A, p, r)
-    print('q={0}'.format(q))
+    # print('q={0}'.format(q))
     quicksort(A, p, q - 1)
     quicksort(A, q + 1, r)
 
+
+
+def randomized_partition(A, p, r):
+  """
+    randomized edition of partition
+  """
+  i = random.randrange(p, r + 1)
+  # print('i = ', i)
+  A[r], A[i] = A[i], A[r]
+  return partition(A, p, r)
+
+def randomized_quicksort(A, p, r):
+  """
+    randomized edition of quicksort
+  """
+  if p < r:
+    q = randomized_partition(A, p, r)
+    # print('q={0}'.format(q))
+    randomized_quicksort(A, p, q - 1)
+    randomized_quicksort(A, q + 1, r)
 
 
 
--- sample/test_quicksort.py
+++ sample/test_quicksort.py
@@ -7,9 +7,12 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath('..'))
 
-from python.quick_sort import quicksort
+from python.quick_sort import quicksort, randomized_quicksort
 
 A = [2,8,7,1,3,5,6,4]
 print("the orginal array: ", A)
 quicksort(A, 0, len(A) - 1)
-print("after: ", A)
+print("after quick sort: ", A)
+B = [2,8,7,1,3,5,6,4]
+randomized_quicksort(B, 0, len(B) - 1)
+print("after randomized quick sort: ", B)
