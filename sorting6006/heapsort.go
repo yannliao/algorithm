f796b4d87e4c0354c1e5bff4d4b34db4049cf439
yannliao <liaoyann@gmail.com>
2019-09-02 17:39:56 +0800

    pointer

--- sorting6006/heapsort.go
+++ sorting6006/heapsort.go
@@ -10,40 +10,41 @@ func right(i int) int {
 	return 2 * (i + 1)
 }
 
-func maxheapify(s *[]int, size int, i int) {
-	// p := parent(i)
+func maxheapify(p *[]int, size int, i int) {
+	s := *p
 	l := left(i)
 	r := right(i)
-	// length := len(*s)
 	max := i
-	if l < size && (*s)[max] < (*s)[l] {
+	if l < size && s[max] < s[l] {
 		max = l
 	}
-	if r < size && (*s)[max] < (*s)[r] {
+	if r < size && s[max] < s[r] {
 		max = r
 	}
 	if max != i {
-		(*s)[i], (*s)[max] = (*s)[max], (*s)[i]
-		maxheapify(s, size, max)
+		s[i], s[max] = s[max], s[i]
+		maxheapify(p, size, max)
 	}
 }
 
-func buidmaxheap(s *[]int) {
-	size := len(*s)
-	i := len(*s)/2 - 1
+func buidmaxheap(p *[]int) {
+	s := *p
+	size := len(s)
+	i := len(s)/2 - 1
 	for i > -1 {
-		maxheapify(s, size, i)
+		maxheapify(p, size, i)
 		i--
 	}
 }
 
 //HeapSort algorithm
-func HeapSort(s *[]int) {
-	buidmaxheap(s)
-	size := len(*s)
+func HeapSort(p *[]int) {
+	s := *p
+	buidmaxheap(p)
+	size := len(s)
 	for i := size - 1; i > 0; i-- {
-		(*s)[0], (*s)[i] = (*s)[i], (*s)[0]
+		(s)[0], (s)[i] = (s)[i], (s)[0]
 		size--
-		maxheapify(s, size, 0)
+		maxheapify(p, size, 0)
 	}
 }
